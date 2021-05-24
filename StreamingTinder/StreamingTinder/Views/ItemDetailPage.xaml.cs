26f598a0bb22c61a9cb1b0d330a77c929a950928
benjatikki <benjamin.tikki@gmail.com>
2021-05-24 12:45:49 +0200

    InitialCommit

---
+++ StreamingTinder/StreamingTinder/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using StreamingTinder.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace StreamingTinder.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
